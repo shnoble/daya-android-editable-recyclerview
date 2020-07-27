a507b05720b7e024be65a90d967dff661cd5b725
shhong <shnoble79@gmail.com>
2020-07-27 18:32:53 +0900

    Implement base recycler view

--- app/src/main/java/com/daya/android/recyclerview/MainFragment.kt
+++ app/src/main/java/com/daya/android/recyclerview/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -18,12 +19,28 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val rootView = inflater.inflate(R.layout.fragment_main, container, false)
-        rootView?.let { setupRecyclerView(rootView) }
+        rootView?.let {
+            setupRecyclerView(it)
+            setupMenu(it)
+        }
         return rootView
     }
 
-    private fun setupRecyclerView(rootView: View) {
-        rootView.findViewById<RecyclerView>(R.id.recycler_view).apply {
+    private fun setupMenu(view: View) {
+        val editButton = view.findViewById<Button>(R.id.edit_button)
+        val completeButton = view.findViewById<Button>(R.id.complete_button)
+        editButton.setOnClickListener {
+            editButton.visibility = View.GONE
+            completeButton.visibility = View.VISIBLE
+        }
+        completeButton.setOnClickListener {
+            editButton.visibility = View.VISIBLE
+            completeButton.visibility = View.GONE
+        }
+    }
+
+    private fun setupRecyclerView(view: View) {
+        view.findViewById<RecyclerView>(R.id.recycler_view).apply {
             // use this setting to improve performance if you know that changes
             // in content do not change the layout size of the RecyclerView
             setHasFixedSize(true)
